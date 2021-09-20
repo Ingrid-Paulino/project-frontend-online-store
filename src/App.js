15d10e5781b83d312fc09375d1e68c233681a15e
Saulo Lima <saulolima.c@protonmail.com>
2021-09-20 16:21:03 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Card from './pages/Card';
 
 export default class App extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class App extends Component {
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={ HomePage } />
+            <Route path="/card" component={ Card } />
           </Switch>
         </BrowserRouter>
       </div>
